14bddc266af4e80aa2d5c7ceabb6a72e3c54db97
Law-son <buabassahlawson@gmail.com>
2024-05-01 10:24:29 +0000

    express router updated

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const authenticationRouter = require('./routes/authentication.route');
 const homeScreenDataRouter = require('./routes/homeScreenData.route');
-const tutorialVideoRouter = require('./routes/tutorialVideo.route');
-const tutorialServiceRouter = require('./routes/tutorialService.route');
-const tutorRouter = require('./routes/tutor.route');
-const reviewRouter = require('./routes/review.route');
+const manageTutorialRouter = require('./routes/manageTutorials.route');
 const paymentRouter = require('./routes/payment.route');
-
+const profileRouter = require('./routes/profile.route');
+const reviewRouter = require('./routes/review.route');
+const requestRouter = require('./routes/tutorialRequests.route');
+ 
 
 const app = express();
 
@@ -15,11 +15,12 @@ app.use(bodyParser.json());
 
 app.use('/home', homeScreenDataRouter);
 app.use('/authentication/', authenticationRouter);
-app.use('/videos/', tutorialVideoRouter);
-app.use('/services/', tutorialServiceRouter);
-app.use('/tutors/', tutorRouter);
-app.use('/reviews/', reviewRouter);
+app.use('/manage/', manageTutorialRouter);
 app.use('/payment/', paymentRouter);
+app.use('/profile/', profileRouter);
+app.use('/reviews/', reviewRouter);
+app.use('/requests/', requestRouter);
+
 
 module.exports = app;
 
