3032359106f7cbf0c67f3f47b3422bca3e99e177
Law-son <buabassahlawson@gmail.com>
2024-05-03 09:09:56 +0000

    set new mongodb collection

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connection = mongoose.createConnection('mongodb://127.0.0.1:27017/Tutorium-Tutor').on('open', ()=>{
+const connection = mongoose.createConnection('mongodb+srv://eyarko:%40Kwesi500@lawsonscluster.l1vqepq.mongodb.net/Tutorium-Tutor').on('open', ()=>{
     console.log("Database connected successfully");
 }).on('error', ()=>{
     console.log("Error connecting to database");
