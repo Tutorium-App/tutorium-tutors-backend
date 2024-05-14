fb416e9ae5ac3004239732aa842d0f120f52285b
Law-son <buabassahlawson@gmail.com>
2024-05-14 00:14:48 +0000

    updated tutor model

--- models/tutor.model.js
+++ models/tutor.model.js
@@ -5,56 +5,75 @@ const { Schema } = mongoose;
 const tutorSchema = new Schema({
     tutorID: {
         type: String,
+        unique: true,
+        required: true
     },
     fullName: {
         type: String,
+        required: true
     },
     email: {
         type: String,
+        unique: true,
+        required: true
     },
     phone: {
         type: String,
+        required: true
     },
     dateCreated: {
-        type: String,
+      type: String,
+      required: true
     },
     school: {
         type: String,
+        required: true
     },
     program: {
         type: String,
+        required: true
     },
     year: {
         type: String,
+        required: true
     },
     about: {
         type: String,
+        required: true
     },
     numberOfVideos: {
-        type: Number,
+      type: Number,
+      required: true
     },
     numberOfServices: {
-        type: Number,
+      type: Number,
+      required: true
     },
     balance: {
         type: Number,
+        required: true
     },
     rating: {
         type: Number,
+        required: true
     },
     numberOfRatings: {
-        type: Number,
+      type: Number,
+      required: true
     },
     sales: {
-        type: Number,
+      type: Number,
+      required: true
     },
     verified: {
         type: String,
+        required: true
     },
     profilePhotoLink: {
         type: String,
+        required: true
     }
-});
+}, {timestamps: true});
 
 const tutorModel = db.model('Tutors', tutorSchema);
 
