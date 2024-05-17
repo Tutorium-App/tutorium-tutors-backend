97c083d37520134163006c42f6342a748c0a6aed
Law-son <buabassahlawson@gmail.com>
2024-05-17 19:06:46 +0000

    updated emails

--- services/tutorialRequest.services.js
+++ services/tutorialRequest.services.js
@@ -35,15 +35,15 @@ class TutorialRequestServices {
 
             // Send student email if tutorial request is accepted
             if (newAcceptedRequest) {
-                const message = `Dear ${studentName},\n\n
-                Good news! ${tutor} has just accepted your request and is eager to help you with the role: "${role}"\n\n
-                You can review this tutor's profile on our app. This can help you decide whether the tutor fits your educational needs based on their qualifications, teaching style, and previous reviews from other students.\n\n
-                Take your time to make an informed decision. Once you are ready, you can confirm your preferred tutor directly through the app.\n\n
-                If you have any questions or need further assistance in making your choice, please don't hesitate to reach out. We are here to support you every step of the way.\n\n
-                Thank you for using Tutorium. We are thrilled to be a part of your educational journey!\n\n
-                Warm regards,\n
-                The Tutorium Team`;
-
+                const message = `Dear ${studentName},\n
+                Good news! ${tutor} has just accepted your request and is eager to help you with the role: "${role}"
+                You can review this tutor's profile on our app. This can help you decide whether the tutor fits your educational needs based on their qualifications, teaching style, and previous reviews from other students.
+                Take your time to make an informed decision. Once you are ready, you can confirm your preferred tutor directly through the app.
+                If you have any questions or need further assistance in making your choice, please don't hesitate to reach out. We are here to support you every step of the way.
+                Thank you for using Tutorium. We are thrilled to be a part of your educational journey!\n
+                Warm regards,
+                The Tutorium Team \n
+                [Customer service email: tutorium.customer@gmail.com. Email us here.]`;
                 const subject = "Your Tutorial Request has been Accepted!";
 
                 // Assuming sendEmail is a function within your EmailServices class
